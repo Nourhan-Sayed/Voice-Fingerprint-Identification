14d6aef824e1449f1417b9ba92a8461a1a44f400
Omar-Saad-ELGharbawy <84602951+Omar-Saad-ELGharbawy@users.noreply.github.com>
2022-12-10 15:27:05 +0200

    final2

--- app.py
+++ app.py
@@ -6,50 +6,39 @@ import os
 import wave
 import pyaudio
 
-
-def record_audio_test():
-	FORMAT = pyaudio.paInt16
-	CHANNELS = 1
-	RATE = 44100
-	CHUNK = 512
-	RECORD_SECONDS = 2
-	audio = pyaudio.PyAudio()
-	index = 1	
-	stream = audio.open(format=FORMAT, channels=CHANNELS,
-				rate=RATE, input=True,input_device_index = index,
-				frames_per_buffer=CHUNK)
-	Recordframes = []
-	for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-		data = stream.read(CHUNK)
-		Recordframes.append(data)
-	stream.stop_stream()
-	stream.close()
-	audio.terminate()
-	OUTPUT_FILENAME="Record.wav"
-	WAVE_OUTPUT_FILENAME=os.path.join("record",OUTPUT_FILENAME)
-	trainedfilelist = open("record.txt", 'a')
-	trainedfilelist.write(OUTPUT_FILENAME+"\n")
-	waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-	waveFile.setnchannels(CHANNELS)
-	waveFile.setsampwidth(audio.get_sample_size(FORMAT))
-	waveFile.setframerate(RATE)
-	waveFile.writeframes(b''.join(Recordframes))
-	waveFile.close()
-    
+from functions import *
 
 
 # import model
 flasklink = Flask(__name__)
 # model = pickle.load(open('MachineLearning.pkl','rb'))
-
+sentence_flag = False
+# team_flag= False
 
 @flasklink.route('/')
 def index():
-  return render_template('Home.html',custom_css = "home")
+	return render_template('Home.html',custom_css = "home")
 
 @flasklink.route('/link',  methods=['GET', 'POST'])
 def link():
+
     record_audio_test()
+
+    team_flag ,member_name = test_model("people")
+    if (team_flag) :
+        print("Access Allowed")
+        _, sentence = test_model("sentence")
+        if sentence == "Open_The_Door_Omar" :
+            print("OPEN")
+            sentence_flag =True
+        elif sentence == "Close_The_Door_Omar" :
+            print("Close")
+            sentence_flag =False
+        else :
+            print("Nothing")
+            sentence_flag =False
+    else :
+        print("Access Denied")
     return render_template('Home.html', custom_css = "home")
 
 
